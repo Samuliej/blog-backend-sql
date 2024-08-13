ac028bbd44bb96f344d7c88de9fdb24c8e999745
Samuli Toppi <samuli.toppi@outlook.com>
2024-08-13 11:50:01 +0300

    Tehtävä 13.7, complete

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -13,53 +13,40 @@ const blogFinder = async (req, res, next) => {
 /* GET ROUTES */
 
 router.get('/', async (req, res) => {
-  try {
-    const blogs = await Blog.findAll()
-    res.status(200).json(blogs)
-  } catch (error) {
-    res.status(404).json({ error: `Blogs not found: ${error}` })
-  }
+  const blogs = await Blog.findAll()
+  res.status(200).json(blogs)
 })
 
-router.get('/:id', blogFinder, async (req, res) =>
-  req.blog
-    ? res.status(200).json(req.blog)
-    : res.status(404).json({ error: `Something went wrong finding the blog` }))
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog)
+    res.status(200).json(req.blog)
+})
 
 /* POST ROUTES */
 
 router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.status(201).json(blog)
-  } catch (error) {
-    res.status(500).json({ error: `Something went wrong creating the blog: ${error}` })
-  }
+  const blog = await Blog.create(req.body)
+  res.status(201).json(blog)
 })
 
 /* PUT ROUTES */
 
 router.put('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
-    try {
-      let likedBlog = req.blog
-      likedBlog.likes++
-      await likedBlog.save()
-      res.status(200).json({ likes: likedBlog.likes })
-    } catch (error) {
-      res.status(500).json({ error: `Something went wrong updating the blog: ${error}` })
-    }
+    let likedBlog = req.blog
+    likedBlog.likes++
+    await likedBlog.save()
+    res.status(200).json({ likes: likedBlog.likes })
   }
 })
 
 /* DELETE ROUTES */
 
 router.delete('/:id', blogFinder, async (req, res) => {
+  const id = req.params.id
   if (req.blog) {
-      await Blog.destroy({ where: { id: id } })
-      res.status(200).json({ message: `Blog ${blogToDelete.title} deleted succesfully.` })
-  } else {
-    res.status(500).json({ error: `Something went wrong deleting the blog: ${error}` })
+    await Blog.destroy({ where: { id: id } })
+    res.status(200).json({ message: `Blog ${req.blog.title} deleted succesfully.` })
   }
 })
 
